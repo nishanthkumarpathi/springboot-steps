33906d7a7b0f49f29b991ad0d985f9fcd0e62b56
Nishanth Kumar Pathi <pathinishanth@gmail.com>
2022-12-02 14:19:05 +0000

    Stage 1

--- src/main/java/com/bbk/springbootdemo/controller/HomeController.java
+++ src/main/java/com/bbk/springbootdemo/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.bbk.springbootdemo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,9 @@ public class HomeController {
         return "This is the Default Root Response!";
     }
 
-    @RequestMapping("/user")
+    //  @RequestMapping(value = "/user", method = RequestMethod.GET)
+    // Stearo Type Methods
+    @GetMapping("/user")
     public User user() {
         User user = new User();
         user.setId("1");
