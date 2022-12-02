f4abf8486db739ab783170bc23620274c72b8dee
Nishanth Kumar Pathi <pathinishanth@gmail.com>
2022-12-02 15:49:29 +0000

    21. Implementing GET Method for Employee Resource

--- src/main/java/com/bbk/springbootdemo/service/EmployeeService.java
+++ src/main/java/com/bbk/springbootdemo/service/EmployeeService.java
@@ -2,7 +2,11 @@ package com.bbk.springbootdemo.service;
 
 import com.bbk.springbootdemo.model.Employee;
 
+import java.util.List;
+
 public interface EmployeeService {
 
     Employee save(Employee employee);
+
+    List<Employee> getAllEmployees();
 }
--- src/main/java/com/bbk/springbootdemo/service/EmployeeServiceImpl.java
+++ src/main/java/com/bbk/springbootdemo/service/EmployeeServiceImpl.java
@@ -19,4 +19,9 @@ public class EmployeeServiceImpl implements EmployeeService {
             employees.add(employee);
             return employee;
         }
+
+    @Override
+        public List<Employee> getAllEmployees() {
+            return employees;
+        }
 }
