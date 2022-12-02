6df8cea0986d1bcfca48c8f9917092cca12b1dbf
Nishanth Kumar Pathi <pathinishanth@gmail.com>
2022-12-02 12:59:09 +0000

    Defualt Hello World

---
+++ src/main/java/com/bbk/springbootdemo/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.bbk.springbootdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World!";
+    }
+
+}
