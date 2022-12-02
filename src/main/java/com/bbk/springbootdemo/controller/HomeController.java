10889d4d43c4901a952b0e8f328332de7907b2c3
Nishanth Kumar Pathi <pathinishanth@gmail.com>
2022-12-02 14:13:21 +0000

    Stage 1

--- src/main/java/com/bbk/springbootdemo/controller/HomeController.java
+++ src/main/java/com/bbk/springbootdemo/controller/HomeController.java
@@ -1,16 +1,31 @@
 package com.bbk.springbootdemo.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@ResponseBody
+import com.bbk.springbootdemo.model.User;
+
+//@Controller
+//@ResponseBody
+
+// @RestController = @Controller + @ResponseBody
+
+@RestController
 public class HomeController {
-
     @RequestMapping("/")
     public String home() {
-        return "Hello World!";
+        return "This is the Default Root Response!";
+    }
+
+    @RequestMapping("/user")
+    public User user() {
+        User user = new User();
+        user.setId("1");
+        user.setName("Nishanth");
+        user.setEmailId("pathinishanth@gmail.com");
+
+        return user;
     }
 
 }
+
